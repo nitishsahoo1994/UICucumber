08a8b8684df34e141f6a3ea6ef9f913889f80329
Nitish kumar Sahoo <49935300+nitishsahoo1994@users.noreply.github.com>
2025-12-04 10:13:27 +0530

    Clean up PropertiesRead.java formatting
    Removed unnecessary whitespace and comments in PropertiesRead.java.

--- src/test/java/utils/PropertiesRead.java
+++ src/test/java/utils/PropertiesRead.java
@@ -6,20 +6,13 @@ import java.io.IOException;
 import java.util.Properties;
 
 public class PropertiesRead {
-	
-	
 	public static void main(String[] args) throws IOException {
 		getProperty();
 	}
-	
-	
 	public static void getProperty() throws IOException {
-		
 		Properties prop=new Properties();
 		FileInputStream fis=new FileInputStream("C:\\Users\\nsah\\APIDemo\\MonitorUICucumber\\src\\test\\resources\\Config.properties");
 		prop.load(fis);
 		System.out.println(prop.getProperty("url"));
-		
 	}
-
 }
